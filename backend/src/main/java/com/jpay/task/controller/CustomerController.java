03869fc9f89a617052c995adb9b9e2af86645369
Hossameldin Youssef <hossameldin.youssef94@gmail.com>
2021-12-25 22:41:42 +0200

    add frontend project

--- backend/src/main/java/com/jpay/task/controller/CustomerController.java
+++ backend/src/main/java/com/jpay/task/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.jpay.task.data.Customer;
 import com.jpay.task.models.CustomerModel;
 import com.jpay.task.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,8 @@ public class CustomerController {
     public List<Customer> readCustomers(){
         return customerService.readCustomers();
     }
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/getcountrycustomers", method = RequestMethod.GET)
     public List<CustomerModel> getCountryCustomer(){
         return customerService.getCustomers();
---
+++ frontend/vueapp/src/components/Customers.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <h3>Customers:</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Country</th>
+          <th scope="col">State</th>
+          <th scope="col">Phone Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="customer in customers" v-bind:key="customer.id"> 
+          <th scope="row">{{customer.id}}</th>
+          <td>{{customer.name}}</td>
+          <td>{{customer.countryName}}</td>
+          <td>{{customer.valid}}</td>
+          <td>{{customer.phoneNumber}}</td>
+        </tr>
+      </tbody>
+    </table> 
+  </div> 
+</template>
+<script>
+  import axios from 'axios';
+  export default {
+    name: 'Customers',
+    data() {
+      return {
+        customers: null,
+      };
+      }
+      ,
+    created: function() {
+      axios
+        .get('http://localhost:8080/getcountrycustomers')
+        .then(res => {
+          this.customers = res.data;
+        })
+    }
+  }
+</script>
+<style>
+  h3 {
+    margin-bottom: 5%;
+  }
+</style>
